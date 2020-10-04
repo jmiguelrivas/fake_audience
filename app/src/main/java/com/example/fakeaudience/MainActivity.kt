bf5d5a236d11c39ae4b38fc9d760d9fe25b3d664
Miguel <jemiguelrivas@gmail.com>
2020-10-04 19:59:21 -0400

    playing simple sound

--- app/src/main/java/com/example/fakeaudience/MainActivity.kt
+++ app/src/main/java/com/example/fakeaudience/MainActivity.kt
@@ -3,16 +3,25 @@ package com.example.fakeaudience
 import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
-    MediaPlayer fxApplauses;
+    private lateinit var mp: MediaPlayer
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        fxApplauses=MediaPlayer.create(MainActivity.this, R.raw.applauses)
+        mp = MediaPlayer.create(this, R.raw.applauses)
+        mp.isLooping = false
+        mp.setVolume(1.0f, 1.0f)
     }
-    public void playApplauses(View v){
-        fxApplauses.start();
+
+    fun playApplauses(v: View) {
+        if (mp.isPlaying) {
+            mp.pause()
+        } else {
+            mp.seekTo(0);
+            mp.start()
+        }
     }
 }
