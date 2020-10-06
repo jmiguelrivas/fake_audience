5f89d77f920d548a0529971004b1e7f5eacb5559
Miguel <jemiguelrivas@gmail.com>
2020-10-06 11:05:36 -0400

    playing sounds again

--- app/src/main/java/com/example/fakeaudience/GridItem.kt
+++ app/src/main/java/com/example/fakeaudience/GridItem.kt
@@ -1,11 +1,11 @@
 package com.example.fakeaudience
 
 import android.media.MediaPlayer
+//import android.os.Bundle
+//import android.view.View
 import android.content.Context
-import android.os.Bundle
 import android.util.AttributeSet
 import android.view.LayoutInflater
-import android.view.View
 import android.widget.LinearLayout
 import android.widget.Button
 import android.widget.TextView
@@ -13,22 +13,23 @@ import android.widget.TextView
 class GridItem(context: Context, attrs: AttributeSet) : LinearLayout(context, attrs) {
     data class Fx(var title: String, var icon:String, var audio:Any)
 
-    private lateinit var mp: MediaPlayer
+//    private var mp: MediaPlayer? = null
     private val fx_title: TextView
     private val fx_btn: Button
     private val Fxs = listOf<Fx>(
-        Fx("Applauses","\uf2b5", R.raw.applauses),
+        Fx("Applause","\uf2b5", R.raw.applauses),
         Fx("Crickets","\uf2b5", R.raw.applauses),
         Fx("Boo","\uf2b5", R.raw.applauses),
         Fx("Aww!","\uf2b5", R.raw.applauses),
-        Fx("Rimshot","\uf2b5", R.raw.applauses),
         Fx("Laughs","\uf2b5", R.raw.applauses),
         Fx("Meow!","\uf2b5", R.raw.applauses),
+        Fx("Evil Laughter", "\uf2b5", R.raw.applauses),
+        Fx("Rimshot","\uf2b5", R.raw.applauses),
         Fx("Burp","\uf2b5", R.raw.applauses),
         Fx("Fart","\uf2b5", R.raw.applauses),
-        Fx("Drumroll","\uf2b5", R.raw.applauses),
-        Fx("Evil Laughter", "\uf2b5", R.raw.applauses)
+        Fx("Drumroll","\uf2b5", R.raw.applauses)
     )
+//    private var currentSong: MutableList<Int> = mutableListOf(R.raw.applauses)
 
     init {
         val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
@@ -37,23 +38,13 @@ class GridItem(context: Context, attrs: AttributeSet) : LinearLayout(context, at
         fx_btn = view.findViewById(R.id.fx_btn)
         fx_title.text = Fxs[5].title
         fx_btn.text = Fxs[5].icon
-//        mp = MediaPlayer.create(R.layout.activity_main, Fxs[5].audio)
+//        controlSound(currentSong[0])
     }
 
-//    fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        setContentView(R.layout.grid_item)
-//        mp = MediaPlayer.create(this, R.raw.applauses)
-//        mp.isLooping = false
-//        mp.setVolume(1.0f, 1.0f)
+
+//    private fun controlSound(id: Int) {
+//        if(mp == null) {
+//            mp = MediaPlayer.create(this, id)
+//        }
 //    }
-
-    fun playApplauses(v: View) {
-        if (mp.isPlaying) {
-            mp.pause()
-        } else {
-            mp.seekTo(0);
-            mp.start()
-        }
-    }
 }
--- app/src/main/java/com/example/fakeaudience/MainActivity.kt
+++ app/src/main/java/com/example/fakeaudience/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.fakeaudience
 
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.grid_item.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity: AppCompatActivity() {
+    private var mp: MediaPlayer? = null
+    private var currentSong: MutableList<Int> = mutableListOf(R.raw.applauses)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        controlSound(currentSong[0])
+    }
+
+    private fun controlSound(id: Int) {
+        fx_btn.setOnClickListener {
+            if(mp == null) {
+                mp = MediaPlayer.create(this, id)
+            }
+            mp?.start()
+        }
     }
 }
