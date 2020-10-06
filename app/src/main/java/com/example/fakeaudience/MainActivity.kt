7427e249a00a3acad279c94ba4dbf0b984d99511
Miguel <jemiguelrivas@gmail.com>
2020-10-05 20:28:09 -0400

    layout refactoring

--- app/src/main/java/com/example/fakeaudience/MainActivity.kt
+++ app/src/main/java/com/example/fakeaudience/MainActivity.kt
@@ -1,25 +1,11 @@
 package com.example.fakeaudience
 
-import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var mp: MediaPlayer
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        mp = MediaPlayer.create(this, R.raw.applauses)
-    }
-
-    fun playApplauses(v: View) {
-        if (mp.isPlaying) {
-            mp.pause()
-        } else {
-            mp.seekTo(0);
-            mp.start()
-        }
     }
 }
--- readme.md
+++ readme.md
@@ -1,3 +1,22 @@
-I was just looking for someone who laughs at my jokes in this pandemic.
+> _"To be honest, I was just looking for someone who laughs at my jokes in this pandemic."_\
+-- ___Miguel Rivas, such a poet___
 
-¯\_(ツ)_/¯
+# Sounds
+- Applauses
+- Crickets
+- Boo
+- Aww! (Disappointment)
+- Rimshot
+- Laughs
+- Meow!
+- Burp
+- Fart
+- Drumroll
+- Evil Laughter
+
+# White Noises
+- Thunder
+- Fireplace
+- Waves
+- Rain
+- Nevada Dream
