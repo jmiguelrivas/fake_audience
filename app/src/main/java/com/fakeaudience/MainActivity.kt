9858d5c0d435642d06261add3b725312c98f89d0
Miguel <jemiguelrivas@gmail.com>
2020-10-25 00:29:55 -0400

    cleaning

--- app/src/main/java/com/fakeaudience/MainActivity.kt
+++ app/src/main/java/com/fakeaudience/MainActivity.kt
@@ -8,20 +8,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val list : MutableList<String> = ArrayList()
-        list.add("Kotlin")
-        list.add("Java")
-        list.add("C++")
-        list.add("Html")
-        list.add("JavaScript")
-        list.add("Php")
-
-//        customview.setData(list)
-//        customview.setTitle("Languages")
-//        submit.setOnClickListener{
-//            Toast.makeText(this,"Selected Datas : " + customview.getSelectedData().toString(),Toast.LENGTH_SHORT).show()
-//        }
-
     }
 }
--- app/src/main/java/com/fakeaudience/MyCustomView.kt
+++ app/src/main/java/com/fakeaudience/MyCustomView.kt
@@ -7,76 +7,49 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.*
 
-/**
- * Created by VickY on 2017-10-28.
- */
-
 class MyCustomView(context: Context, attrs: AttributeSet) : LinearLayout(context, attrs) {
 
-    private var autocompletextview: AutoCompleteTextView
     private var listview: ListView
     private var title: TextView
-    private var add: ImageView
+    private var play_btn: ImageView
 
     private var selectedItems: MutableList<String> = ArrayList()
-    private var allItems: MutableList<String> = ArrayList()
+
+    data class Fx(var title: String, var icon:String, var audio:Int)
+    val Fxs = listOf<Fx>(
+            Fx("Applause","\uf2b5", R.raw.applauses),
+            Fx("Crickets","\uf2b5", R.raw.applauses),
+            Fx("Boo","\uf2b5", R.raw.applauses),
+            Fx("Aww!","\uf2b5", R.raw.applauses),
+            Fx("Laughs","\uf2b5", R.raw.applauses),
+            Fx("Meow!","\uf2b5", R.raw.applauses),
+            Fx("Evil Laughter", "\uf2b5", R.raw.applauses),
+            Fx("Rimshot","\uf2b5", R.raw.applauses),
+            Fx("Burp","\uf2b5", R.raw.applauses),
+            Fx("Fart","\uf2b5", R.raw.applauses),
+            Fx("Drumroll","\uf2b5", R.raw.applauses)
+    )
 
     init {
         val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
         val view = inflater.inflate(R.layout.my_custom_view_layout, this, true)
-        autocompletextview = view.findViewById(R.id.autocompletetextview)
         listview = view.findViewById(R.id.listview)
         title = view.findViewById(R.id.title)
-        add = view.findViewById(R.id.add)
+        play_btn = view.findViewById(R.id.play_btn)
 
-        autocompletextview.threshold = 1
-
-        add.setOnClickListener {
-            val selectedString = autocompletextview.text.trim().toString()
-            when {
-                selectedString.isEmpty() -> Toast.makeText(getContext(), "Please enter data.", Toast.LENGTH_SHORT).show()
-                selectedItems.contains(selectedString) -> Toast.makeText(getContext(), "Item already added.", Toast.LENGTH_SHORT).show()
-                else -> {
-                    selectedItems.add(0, selectedString)
-                    refreshData(true)
-                }
-            }
+        for (item in Fxs) {
+            selectedItems.add(0, item.title)
         }
-
-    }
-
-    fun setData(data: MutableList<String>) {
-        allItems = data
-        autocompletextview.setAdapter(ArrayAdapter(context, android.R.layout.simple_list_item_1, allItems))
-    }
-
-    fun setTitle(str: String) {
-        title.text = str
-    }
-
-    fun getSelectedData(): MutableList<String> {
-        return selectedItems
+        refreshData(true)
     }
 
     fun refreshData(clearData: Boolean) {
         listview.adapter = MyCustomViewAdapter(context, R.layout.my_custom_view_item, selectedItems)
-        setListViewHeightBasedOnChildren(listview)
-        if (clearData)
-            autocompletextview.setText("")
     }
 
-    private fun setListViewHeightBasedOnChildren(listView: ListView) {
-        val listAdapter = listView.adapter ?: return
-        var totalHeight = listView.paddingTop + listView.paddingBottom
-        for (i in 0 until listAdapter.count) {
-            val listItem = listAdapter.getView(i, null, listView)
-            (listItem as? ViewGroup)?.layoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT)
-            listItem.measure(0, 0)
-            totalHeight += listItem.measuredHeight
-        }
-        val params = listView.layoutParams
-        params.height = totalHeight + listView.dividerHeight * (listAdapter.count - 1)
-        listView.layoutParams = params
+    private fun playSound(position:Int) {
+        selectedItems.removeAt(position)
+        refreshData(false)
     }
 
     inner class MyCustomViewAdapter(context: Context?, var resource: Int, var objects: MutableList<String>?) : ArrayAdapter<String>(context, resource, objects) {
@@ -91,8 +64,7 @@ class MyCustomView(context: Context, attrs: AttributeSet) : LinearLayout(context
             val delete = view.findViewById<ImageView>(R.id.delete)
             name.text = objects!!.get(position)
             delete.setOnClickListener {
-                selectedItems.removeAt(position)
-                refreshData(false)
+                playSound(position)
             }
             return view
         }
